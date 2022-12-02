a3b51272a19a5859751736b31ca096461344927d
Caitlyn D <sablueeyes1@gmail.com>
2022-12-02 11:13:45 -0600

    Update

--- mortgage-calc/js/script.js
+++ mortgage-calc/js/script.js
@@ -15,7 +15,7 @@ function doMath(){
    var m = document.getElementById("mon").value;
                m=Number(m);
    var n = 12;
-     var B = P * (1 + r/n)**n*t;
+     var B = P * (1 + r/n)**(n*t);
      var nroot = m * (1 + r/n)**n*t -1 / r/n;
 
      var out= document.getElementById("out");
