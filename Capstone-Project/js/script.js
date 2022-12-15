698fa1a6183755916da88ee9f58eb20ead56acc5
Caitlyn D <sablueeyes1@gmail.com>
2022-12-15 11:13:30 -0600

    Update

--- Capstone-Project/js/script.js
+++ Capstone-Project/js/script.js
@@ -1,5 +1,14 @@
 const images = document.querySelectorAll("img");
 let i=0;
 setInterval(function()){
-    
-}
+   images.ForEach( (img, i) => {
+    img.style.display ='none';
+
+   })
+
+   if (i == images.length){
+    i =0;
+   }
+   images[i].style.display = 'block';
+   i++;
+},2000);
