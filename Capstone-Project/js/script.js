3761326d16cf7a587c5c0ed05aa674614e2806b7
Caitlyn D <sablueeyes1@gmail.com>
2022-12-16 11:13:33 -0600

    Update

--- Capstone-Project/js/script.js
+++ Capstone-Project/js/script.js
@@ -7,7 +7,7 @@ setInterval(function(){
     images[i].style.display = "fade-in-image";
    }else if(i==images.length){
     images[i-1].style.display = "fade-out-image";
-    images[i].style.display = "fade-in-image";
+    images[0].style.display = "fade-in-image";
     i=0;
    }
    else{
