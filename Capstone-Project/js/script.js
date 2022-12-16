08a358bafe3ff2fbab6c578f1c6d2f0436aec3d7
Caitlyn D <sablueeyes1@gmail.com>
2022-12-16 11:15:47 -0600

    Update

--- Capstone-Project/js/script.js
+++ Capstone-Project/js/script.js
@@ -4,15 +4,15 @@ function createImageCarousel(images){
    let i =0;
 setInterval(function(){
    if (i ==0){
-    images[i].style.display = "fade-in-image";
+    images[i].className = "fade-in-image";
    }else if(i==images.length){
-    images[i-1].style.display = "fade-out-image";
-    images[0].style.display = "fade-in-image";
+    images[i-1].className = "fade-out-image";
+    images[0].className = "fade-in-image";
     i=0;
    }
    else{
-    images[i-1].style.display = "fade-out-image";
-    images[i].style.display = "fade-in-image";
+    images[i-1].className = "fade-out-image";
+    images[i].className = "fade-in-image";
    }
    
    i++;
