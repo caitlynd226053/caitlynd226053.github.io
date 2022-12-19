940c2d5b6d0b6582006c34fd0b8b297dd87c36cc
Caitlyn D <sablueeyes1@gmail.com>
2022-12-19 10:37:25 -0600

    Update

--- Capstone-Project/js/script.js
+++ Capstone-Project/js/script.js
@@ -1,23 +1,18 @@
 const homeImages = document.querySelectorAll("#homeImageSlider img");
-function createImageCarousel(images){
-   console.log(images);
-   let i =0;
-setInterval(function(){
-   if (i ==0){
-    //images[images.length-1].className = "fade-out-image";
-      images[i].className = "fade-in-image";
-   }else if(i==images.length){
-    images[i-1].className = "fade-out-image";
-    images[0].className = "fade-in-image";
-    i=0;
-   }
-   else{
-    images[i-1].className = "fade-out-image";
-    images[i].className = "fade-in-image";
-   }
-   
-   i++;
-
-},10000);
+function createImagesCarousel(images) {
+    // Hide all images except the first one
+    for (let i = 1; i < images.length; i++)
+        images[i].style.display = "none";
+    let i = 0;
+    setInterval(function() {
+        images[i].className = "fade-out-image";
+        setTimeout(function() {
+            images[i].style.display = "none";
+            i = (i + 1) % images.length;
+            images[i].className = "fade-in-image";
+            images[i].style.display = "block";
+        }, 1500);  // Cut off the fade out animation .5 seconds early to prevent flickering
+        // Make sure the fadeIn and fadeOut in CSS are 2 seconds (or 500 ms longer)
+    }, 10000);  // Change image every 10 seconds
 }
-createImageCarousel(homeImages);
+createImagesCarousel(homeImages);
