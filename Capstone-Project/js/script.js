71ad7b54c5c0c62897ed02f5130525e923474da9
Caitlyn D <sablueeyes1@gmail.com>
2022-12-19 10:38:56 -0600

    Update

--- Capstone-Project/js/script.js
+++ Capstone-Project/js/script.js
@@ -1,6 +1,5 @@
 const homeImages = document.querySelectorAll("#homeImageSlider img");
 function createImagesCarousel(images) {
-    // Hide all images except the first one
     for (let i = 1; i < images.length; i++)
         images[i].style.display = "none";
     let i = 0;
@@ -11,8 +10,8 @@ function createImagesCarousel(images) {
             i = (i + 1) % images.length;
             images[i].className = "fade-in-image";
             images[i].style.display = "block";
-        }, 1500);  // Cut off the fade out animation .5 seconds early to prevent flickering
-        // Make sure the fadeIn and fadeOut in CSS are 2 seconds (or 500 ms longer)
-    }, 10000);  // Change image every 10 seconds
+        }, 1500); 
+       
+    }, 10000);  
 }
 createImagesCarousel(homeImages);
